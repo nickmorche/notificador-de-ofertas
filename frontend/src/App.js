0f82667df9074b6031910b67ba6a6850e692f5bb
Nicolas Morche <nickmorche@gmail.com>
2025-04-20 23:21:44 -0300

    Adicionado campos no modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,23 @@ import * as React from "react"
 import { Button, Box } from '@chakra-ui/react';
 import  OfertaForm  from "./components/OfertaForm.js";
 import  OfertaList  from './components/OfertaList.js';
-// import ModalCadastro from './components/Modal.js';
+import ModalCadastroOferta from './components/ModalCadastroOferta.js';
 
 
 
 function App(){
+  const handleSubmit = (formData) => {
+    console.log('Dados enviados:', formData);
+    // aqui você pode chamar uma rota backend com axios
+  };
+
+    
   return (
   <div style={{ padding: '20px', fontFamily: 'sans-serif'}}>
       <h1>Cadastro de Ofertas</h1>
-      <OfertaForm />
-      <Button colorScheme="teal" size="md">
-        Teste Chakra
-      </Button>
+      <Box p={5}>
+        <ModalCadastroOferta onSubmit={handleSubmit} />
+      </Box>
       <hr />
       <OfertaList />
   </div>
---
+++ frontend/src/components/ModalCadastroOferta.js
@@ -0,0 +1,172 @@
+import React, { useRef, useState } from 'react';
+import {
+  Button,
+  CloseButton,
+  Dialog,
+  For,
+  HStack,
+  Stack,
+  Field,
+  Input,
+  Kbd,
+  Portal,
+  FormControl
+} from '@chakra-ui/react';
+import axios from 'axios';
+//https://www.chakra-ui.com/docs/components/dialog
+
+const ModalCadastroOferta = () => {
+  const ref = useRef(null)
+  const [open, setOpen] = useState(false)
+  const [formData, setFormData] = React.useState({
+    nomeProduto: '',
+    url: '',
+    marca: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    //if (onSubmit) onSubmit(formData);
+    e.preventDefault();
+    try {
+        axios.post('https://localhost:5000/api/ofertas', formData);
+        alert('Oferta cadastrada!')
+        setFormData({
+            produto: '',
+            url: '',
+            palavraChave: '',
+            marca: '',
+            status: 'ativa'
+        });
+    } catch (err) {
+        alert('Erro ao cadastrar abaixo. Por favor contate a T.I.');
+        console.error(err);
+    }
+    setFormData({ nomeProduto: '', url: '', precoDesejado: '', marca: '' });
+  };
+
+  return (
+    <Dialog.Root key="xl" size="lg" placement="center" motionPreset="slide-in-bottom" open={open} onOpenChange={(e) => setOpen(e.open)}>
+      <Dialog.Trigger asChild>
+        <Button variant="outline" size="sm">
+          Cadastrar produto
+        </Button>
+      </Dialog.Trigger>
+      <Portal>
+        <Dialog.Backdrop />
+        <Dialog.Positioner>
+          <Dialog.Content>
+            <Dialog.Header>
+              <Dialog.Title>Cadastro de Produto</Dialog.Title>
+            </Dialog.Header>
+            <Dialog.Body pb="4">
+              <Stack gap='4'>
+                <Field.Root>
+                  <Field.Label>Produto</Field.Label>
+                  <Input  ref={ref} placeholder="Digite o nome do produto..." />
+                </Field.Root>
+                <Field.Root>
+                  <Field.Label>Marca</Field.Label>
+                  <Input onChange={handleChange} placeholder="Digite o nome da marca..." />
+                </Field.Root>
+                <Field.Root>
+                  <Field.Label>URL</Field.Label>
+                  <Input onChange={handleChange} placeholder="Digite a URL do site..."></Input>
+                </Field.Root>
+              </Stack>
+            </Dialog.Body>
+            <Dialog.Footer>
+              <Dialog.ActionTrigger asChild>
+                <Button variant="outline">Cancel</Button>
+              </Dialog.ActionTrigger>
+              <Button onClick={handleSubmit}>Save</Button>
+            </Dialog.Footer>
+            <Dialog.CloseTrigger asChild>
+              <CloseButton size="sm" />
+            </Dialog.CloseTrigger>
+          </Dialog.Content>
+        </Dialog.Positioner>
+      </Portal>
+    </Dialog.Root>
+  )
+}
+
+
+// const ModalCadastroOferta = ({ onSubmit }) => {
+//   const { isOpen, onOpen, onClose } = useDisclosure();
+//   const [formData, setFormData] = React.useState({
+//     nomeProduto: '',
+//     url: '',
+//     precoDesejado: ''
+//   });
+
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setFormData((prev) => ({ ...prev, [name]: value }));
+//   };
+
+//   const handleSubmit = () => {
+//     if (onSubmit) onSubmit(formData);
+//     onClose();
+//     setFormData({ nomeProduto: '', url: '', precoDesejado: '' });
+//   };
+
+//   return (
+//     <>
+//       <Button onClick={onOpen} colorScheme="teal">Cadastrar Oferta</Button>
+
+//       <Modal isOpen={isOpen} onClose={onClose}>
+//         <ModalOverlay />
+//         <ModalContent>
+//           <ModalHeader>Nova Oferta</ModalHeader>
+//           <ModalCloseButton />
+//           <ModalBody pb={6}>
+//             <FormControl mb={3}>
+//               <FormLabel>Nome do Produto</FormLabel>
+//               <Input
+//                 placeholder="Tênis Nike Air Max"
+//                 name="nomeProduto"
+//                 value={formData.nomeProduto}
+//                 onChange={handleChange}
+//               />
+//             </FormControl>
+
+//             <FormControl mb={3}>
+//               <FormLabel>URL do Produto</FormLabel>
+//               <Input
+//                 placeholder="https://www.exemplo.com/produto"
+//                 name="url"
+//                 value={formData.url}
+//                 onChange={handleChange}
+//               />
+//             </FormControl>
+
+//             <FormControl>
+//               <FormLabel>Preço Desejado (R$)</FormLabel>
+//               <Input
+//                 placeholder="199.90"
+//                 name="precoDesejado"
+//                 type="number"
+//                 value={formData.precoDesejado}
+//                 onChange={handleChange}
+//               />
+//             </FormControl>
+//           </ModalBody>
+
+//           <ModalFooter>
+//             <Button colorScheme="teal" mr={3} onClick={handleSubmit}>
+//               Salvar
+//             </Button>
+//             <Button onClick={onClose}>Cancelar</Button>
+//           </ModalFooter>
+//         </ModalContent>
+//       </Modal>
+//     </>
+//   );
+// };
+
+export default ModalCadastroOferta;
