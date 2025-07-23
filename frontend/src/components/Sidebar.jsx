cbc4ba6bcb042ee6cd7ff6d752971a48d113c35a
Nicolas Morche <nickmorche@gmail.com>
2025-07-22 22:39:59 -0300

    adicionado sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,5 +1,7 @@
-import { Box, VStack, IconButton, Text, Link, useDisclosure } from "@chakra-ui/react";
+import { useState } from 'react';
+import { Box, VStack, Link, IconButton } from "@chakra-ui/react";
 import { NavLink } from "react-router-dom";
+import { FiMenu, FiX } from 'react-icons/fi';
 
 const links = [
     { label: "Ofertas", to: "/" },
@@ -8,7 +10,11 @@ const links = [
 ];
 
 export default function Sidebar() {
-    // const { isOpenSidebar, setOpenSidebar } = useDisclosure(); 
+    const [ isOpen, setIsOpen ] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsOpen(prev => !prev);
+    }
 
     return(
         <Box
@@ -16,29 +22,36 @@ export default function Sidebar() {
             pos="sticky"
             top="0"
             h="100vh"
-            w="64" //{isOpenSidebar ? "64" : "10" }
+            w={ isOpen ? "64" : "20"}
             p="4"
             bg="gray.800"
             color="white"
             shadow="md"
         >
-            <VStack align="start" spacing="4">
-                {links.map((link) => (
-                    <Link
-                        key={link.to}
-                        as={NavLink}
-                        to={link.to}
-                        _hover={{ textDecoration: "none", bg: "gray.700" }}
-                        _activeLink={{ fontWeight: "bold", bg: "gray.700" }}
-                        w="full"
-                        p="2"
-                        borderRadius="md"
-                        color="white"
-                    >
-                        {link.label}
-                    </Link>
-                ))}
-            </VStack>
+            <IconButton 
+                icon={ isOpen ? <FiX/> : <FiMenu/>}
+                onClick={toggleSidebar}
+            />
+            {/* Conteuúdo da Sidebar */}
+            { isOpen && ( 
+                <VStack align="start" spacing="4">
+                    {links.map((link) => (
+                        <Link
+                            key={link.to}
+                            as={NavLink}
+                            to={link.to}
+                            _hover={{ textDecoration: "none", bg: "gray.700" }}
+                            _activeLink={{ fontWeight: "bold", bg: "gray.700" }}
+                            w={ isOpen ? 'full' : '20'}
+                            p="2"
+                            borderRadius="md"
+                            color="white"
+                        >
+                            {link.label}
+                        </Link>
+                    ))}
+                </VStack>
+            )}
         </Box>
 
     )
